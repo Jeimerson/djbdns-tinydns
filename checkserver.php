cb10093272a3ecefd29d54bb8d74a3ac5be2899e
Danny Sheehan <danny@ftmon.org>
2014-03-09 03:03:53 +0000

    fixed some issues

--- checkserver.php
+++ checkserver.php
@@ -4,21 +4,68 @@
  *
  * Create a database and user allow localhost access by that user.
  *
- *  By: Danny Sheehan   http://www.setuptips.com
+ *  By: Danny Sheehan   http://www.ftmon.org
  */
 
-define('DB_NAME',     'db_name');
-define('DB_USER',     'db_user');
-define('DB_PASSWORD', 'db_password');
-define('DB_HOST',     'localhost');
+define('DB_TABLE',     'servers');
 
+define('DB_NAME',     'checkserver');
+define('DB_USER',     'checkserver');
+define('DB_PASSWORD', 'change_me);
+//define('DB_HOST',     'localhost');
+define('DB_HOST',     '127.0.0.1');
+
+
+// Can we connect?
 if ( ! ($conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD)) )
 {
   header("HTTP/1.0 404 Not Found");
   die('Mysql error: ' . mysql_error());
 }
 
+// Can we select?
 if ( ! mysql_select_db(DB_NAME) )
+{
+  header("HTTP/1.0 404 Not Found");
+  die('Mysql error: ' . mysql_error());
+}
+
+$this_host = gethostname();
+$now = time();
+
+// Can we query?
+$sql="SELECT counter FROM " . DB_TABLE . " WHERE name = '$this_host'";
+if ( ! ($result = mysql_query($sql)) )
+{
+  header("HTTP/1.0 404 Not Found");
+  die('Mysql error: ' . mysql_error());
+}
+
+// First time? Then do insert rather than update.
+$num = mysql_numrows($result);
+if ( ! $num )
+{
+  $sql = "INSERT INTO " . DB_TABLE . " ".
+         "(name, counter) ".
+         "VALUES ".
+         "('$this_host', '$now')";
+  if ( ! ($result = mysql_query($sql)) )
+  {
+    header("HTTP/1.0 404 Not Found");
+    die('Mysql error: ' . mysql_error());
+  }
+}
+else
+{ 
+  $last_time = mysql_result($result, 0,"counter");
+}
+
+
+// Can we do an update?
+$sql = "UPDATE " . DB_TABLE . " ".
+       "SET counter = $now " .
+       "WHERE name = '$this_host' ";
+if ( ! ($result = mysql_query($sql)) )
 {
   header("HTTP/1.0 404 Not Found");
   die('Mysql error: ' . mysql_error());
@@ -26,14 +73,18 @@ if ( ! mysql_select_db(DB_NAME) )
 
 mysql_close($conn);
 
+
 //
 // Ensure the monitor script dosn't cache the result.
 // See http://stackoverflow.com/questions/49547/making-sure-a-web-page-is-not-cached-across-all-browsers
 //
+
 header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
 header('Pragma: no-cache'); // HTTP 1.0.
 header('Expires: 0'); // Proxies.
 
-echo "Connected to Database " . gethostname() . "\n";
-?>
+$counter = $now - $last_time;
 
+echo gethostname() . "," . $counter . "\n";
+
+?>
--- monitor-nodes.sh
+++ monitor-nodes.sh
@@ -13,16 +13,24 @@
 TINYDNS="/etc/service/tinydns/root"
 
 # Update this with the IP address of the nodes in your cluster.
-NODES="192.168.1.60 192.168.1.61 192.168.1.62"
+# Or export as environment variables.
+if [ -z "$NODES" ]
+then
+  NODES="192.168.1.2 192.168.1.3 192.168.1.1"
+fi
+
+THIS_HOST=`uname -n`
 
 # The person who gets email when things go wrong.
-ADMIN_MAIL="root"
+ADMIN_MAIL="pager"
 
 # The check script that is called to see if a node is functional or not.
 CHECK_SCRIPT="checkserver.php"
 
 
 DOWN_NODES_FILE="${TINYDNS}/nodes-down.txt"
+touch ${DOWN_NODES_FILE}
+
 DATA="${TINYDNS}/data"
 
 cd $TINYDNS
@@ -45,7 +53,7 @@ NUM_DOWN=`echo $DOWN_NODES | wc -w`
 if [ $NUM_DOWN -eq $MAX_NODES ]
 then
   echo "ERROR: All nodes are down."  | \
-      mail -s "FATAL: all nodes are down" $ADMIN_MAIL 
+      mail -s "$THIS_HOST: FATAL: all nodes are down" $ADMIN_MAIL 
   exit 1
 fi
 
@@ -54,18 +62,30 @@ fi
 #
 if [ $NUM_DOWN -gt 0 ]
 then
-  mv $DATA $DATA.bak
 
   for n in `echo $DOWN_NODES`
   do
-    echo "taking $n out of service." | mail -s "$n down" $ADMIN_MAIL
-    sed -e "s/^\+\([0-9a-zA-Z\-\.]*\):${n}:\([0-9]*\)$/\-\1:${n}:\2/" $DATA.bak > $DATA
-    
+
+    HOST_NAME=`getent hosts $n | awk '{print $2}'`
+
+    # deactive only if down 2 times in a row.
+    if echo $PREV_DOWN | grep -q $n
+    then
+      echo "taking $HOST_NAME out of service." | \
+                  mail -s "$THIS_HOST : $HOST_NAME down for more than 1 count" $ADMIN_MAIL
+      mv $DATA $DATA.bak
+      sed -e "s/^\+\([0-9a-zA-Z\-\.]*\):${n}:\([0-9]*\)$/\-\1:${n}:\2/" $DATA.bak > $DATA
+      # update DNS
+      make
+    else
+      echo "$HOST_NAME is down." | \
+                  mail -s "$THIS_HOST : $HOST_NAME down for 1 count" $ADMIN_MAIL
+    fi
     PREV_DOWN=`echo $PREV_DOWN | sed -e "s/${n}//"`
   done
 fi
 
-
+# These should be nodes that are now up.
 PREV_NUM_DOWN=`echo $PREV_DOWN | wc -w`
 
 #
@@ -73,16 +93,24 @@ PREV_NUM_DOWN=`echo $PREV_DOWN | wc -w`
 #
 if [ $PREV_NUM_DOWN -gt 0 ]
 then
-  mv $DATA $DATA.bak1
   for n in `echo $PREV_DOWN`
   do
-    echo "putting $n back in service." | mail -s "$n up" $ADMIN_MAIL
+    HOST_NAME=`getent hosts $n | awk '{print $2}'`
+    echo "putting $HOST_NAME back in service." | mail -s "$THIS_HOST : $HOST_NAME up" $ADMIN_MAIL
+    mv $DATA $DATA.bak1
     sed -e "s/^\-\([0-9a-zA-Z\-\.]*\):${n}:\([0-9]*\)$/\+\1:${n}:\2/" $DATA.bak1 > $DATA
+    # update DNS
+    make
   done
 fi
 
 echo $DOWN_NODES > $DOWN_NODES_FILE
 
-# update DNS
-make
-
+#
+# start mysql if it is down.
+#
+if ! pgrep -u mysql > /dev/null
+then
+  echo "mysql is down - restarting." | mail -s "$THIS_HOST : Starting mysql" $ADMIN_MAIL
+ service mysql start
+fi 
